49314d123e2d92943e1271eb05ea9c3ffb625ce3
Christian Loehle <christian.loehle@arm.com>
2025-09-30 09:19:34 +0100

    scx_storm: Add scx_storm scheduler
    scx_storm is a scheduler that utilizes both shared runqueues and
    a queuemap to move tasks too. It will then randomly cause migrations
    between them very frequently.
    
    This is to test the sched_ext core code.
    
    Signed-off-by: Christian Loehle <christian.loehle@arm.com>

---
+++ scheds/c/scx_storm.bpf.c
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * A storm scheduler.
+ *
+ * Causes a lot of storm (DSQ migrations)
+ *
+ * Copyright (c) 2025 Arm Limited
+ * Copyright (c) 2025 Christian Loehle <christian.loehle@arm.com>
+ */
+#include <scx/common.bpf.h>
+
+char _license[] SEC("license") = "GPL";
+
+UEI_DEFINE(uei);
+
+#define NR_DSQS 5
+#define MAX_DSQS 5
+
+#define STORM_FACTOR 10
+
+const volatile u32 nr_cpu_ids = 1;
+const volatile unsigned long nr_dsqs_user;
+unsigned long nr_dsqs;
+
+struct {
+	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+	__uint(key_size, sizeof(u32));
+	__uint(value_size, sizeof(u64));
+	__uint(max_entries, MAX_DSQS+2);
+} stats SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_QUEUE);
+	__uint(max_entries, 4096);
+	__type(value, s32);
+} storm_q SEC(".maps");
+
+static void stat_inc(u32 idx)
+{
+	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
+	if (cnt_p)
+		(*cnt_p)++;
+}
+
+static inline unsigned int random(unsigned int n)
+{
+	static unsigned int i = 0;
+	i++;
+	return i % n;
+}
+
+static inline unsigned int random_dsq(void)
+{
+	return random(nr_dsqs);
+}
+
+static inline bool coinflip(unsigned int n)
+{
+	return random(n) % n == 0;
+}
+
+static inline void do_storm(void)
+{
+	struct task_struct *p;
+	unsigned int dsq_src = random_dsq();
+	unsigned int i = 0;
+
+	bpf_for_each(scx_dsq, p, dsq_src, 0) {
+		unsigned int dsq_dst = random_dsq();
+
+		i++;
+		if (dsq_src != dsq_dst) {
+			scx_bpf_dsq_move(BPF_FOR_EACH_ITER, p, dsq_dst, 0);
+			stat_inc(MAX_DSQS);
+		}
+		if (coinflip(STORM_FACTOR) || i >= STORM_FACTOR)
+			break;
+	}
+}
+
+s32 BPF_STRUCT_OPS(storm_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
+{
+	return prev_cpu;
+}
+
+void BPF_STRUCT_OPS(storm_enqueue, struct task_struct *p, u64 enq_flags)
+{
+	s32 pid = p->pid;
+
+	if (coinflip(2) && !bpf_map_push_elem(&storm_q, &pid, 0))
+		return;
+	scx_bpf_dsq_insert(p, random_dsq(), SCX_SLICE_DFL, enq_flags);
+}
+
+void BPF_STRUCT_OPS(storm_dispatch, s32 cpu, struct task_struct *prev)
+{
+	int i;
+
+	do_storm();
+	bpf_for(i, 0, MAX_DSQS) {
+		int random = random_dsq() % nr_dsqs;
+		s32 pid;
+
+		if (i >= nr_dsqs)
+			break;
+		if (!bpf_map_pop_elem(&storm_q, &pid)) {
+			struct task_struct *p = bpf_task_from_pid(pid);
+
+			if (!p) {
+				scx_bpf_error("Lost task_struct with pid %d", pid);
+				return;
+			}
+			stat_inc(MAX_DSQS+1);
+			scx_bpf_dsq_insert(p, random_dsq(), SCX_SLICE_DFL, 0);
+			bpf_task_release(p);
+		}
+		if (scx_bpf_dsq_move_to_local(random)) {
+			stat_inc(random);
+			break;
+		}
+	}
+}
+
+
+void BPF_STRUCT_OPS(storm_running, struct task_struct *p)
+{
+}
+
+void BPF_STRUCT_OPS(storm_stopping, struct task_struct *p, bool runnable)
+{
+	if (coinflip(3))
+		p->scx.slice = 0;
+}
+
+
+void BPF_STRUCT_OPS(storm_tick, struct task_struct *p)
+{
+	if (coinflip(3))
+		p->scx.slice = 0;
+}
+
+void BPF_STRUCT_OPS(storm_enable, struct task_struct *p)
+{
+}
+
+s32 BPF_STRUCT_OPS_SLEEPABLE(storm_init)
+{
+	s32 ret = 0;
+	int i;
+
+	if (nr_dsqs_user > MAX_DSQS)
+		nr_dsqs = MAX_DSQS;
+	else
+		nr_dsqs = nr_dsqs_user;
+
+	bpf_for(i, 0, MAX_DSQS) {
+		if (i >= nr_dsqs)
+			break;
+		ret |= scx_bpf_create_dsq(i, -1);
+		if (ret)
+			return ret;
+	}
+	return ret;
+}
+
+void BPF_STRUCT_OPS(storm_exit, struct scx_exit_info *ei)
+{
+	UEI_RECORD(uei, ei);
+}
+
+SCX_OPS_DEFINE(storm_ops,
+	       .select_cpu		= (void *)storm_select_cpu,
+	       .enqueue			= (void *)storm_enqueue,
+	       .dispatch		= (void *)storm_dispatch,
+	       .running			= (void *)storm_running,
+	       .stopping		= (void *)storm_stopping,
+	       .tick			= (void *)storm_tick,
+	       .enable			= (void *)storm_enable,
+	       .init			= (void *)storm_init,
+	       .exit			= (void *)storm_exit,
+	       .name			= "storm");
---
+++ scheds/c/scx_storm.c
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2025 Arm Limited.
+ * Copyright (c) 2022 Christian Loehle <christian.loehle>
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <assert.h>
+#include <libgen.h>
+#include <bpf/bpf.h>
+#include <scx/common.h>
+#include "scx_storm.bpf.skel.h"
+
+const char help_fmt[] =
+"A sched_ext scheduler that stresses DSQ moves.\n"
+"\n"
+"Usage: %s [-j] [-v]\n"
+"\n"
+"  -j            The number of custom DSQs\n"
+"  -v            Print libbpf debug messages\n"
+"  -h            Display this help and exit\n";
+
+static bool verbose;
+static volatile int exit_req;
+unsigned long nr_dsqs;
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	if (level == LIBBPF_DEBUG && !verbose)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+static void sigint_handler(int simple)
+{
+	exit_req = 1;
+}
+
+#define NR_STATS 7
+
+static void read_stats(struct scx_storm *skel, __u64 *stats)
+{
+	int nr_cpus = libbpf_num_possible_cpus();
+	assert(nr_cpus > 0);
+	__u64 cnts[NR_STATS][nr_cpus];
+	__u32 idx;
+
+	memset(stats, 0, sizeof(stats[0]) * NR_STATS);
+
+	for (idx = 0; idx < NR_STATS; idx++) {
+		int ret, cpu;
+
+		ret = bpf_map_lookup_elem(bpf_map__fd(skel->maps.stats),
+					  &idx, cnts[idx]);
+		if (ret < 0)
+			continue;
+
+		for (cpu = 0; cpu < nr_cpus; cpu++)
+			stats[idx] += cnts[idx][cpu];
+	}
+}
+
+int main(int argc, char **argv)
+{
+	struct scx_storm *skel;
+	struct bpf_link *link;
+	__u32 opt;
+	__u64 ecode;
+
+	libbpf_set_print(libbpf_print_fn);
+	signal(SIGINT, sigint_handler);
+	signal(SIGTERM, sigint_handler);
+restart:
+	skel = SCX_OPS_OPEN(storm_ops, scx_storm);
+
+	while ((opt = getopt(argc, argv, "j:vh")) != -1) {
+		switch (opt) {
+		case 'j':
+			char *end = NULL;
+			nr_dsqs = strtoul(optarg, &end, 0);
+			if (nr_dsqs < 2 || nr_dsqs > 5) {
+				fprintf(stderr, "nr_dsqs must be between 2 and 5, got %lu\n", nr_dsqs);
+				return -EINVAL;
+			}
+			skel->rodata->nr_dsqs_user = nr_dsqs;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		default:
+			fprintf(stderr, help_fmt, basename(argv[0]));
+			return opt != 'h';
+		}
+	}
+
+	if (!nr_dsqs)
+		skel->rodata->nr_dsqs_user = 2;
+	skel->rodata->nr_cpu_ids = libbpf_num_possible_cpus();
+
+	SCX_OPS_LOAD(skel, storm_ops, scx_storm, uei);
+	link = SCX_OPS_ATTACH(skel, storm_ops, scx_storm);
+
+	while (!exit_req && !UEI_EXITED(skel, uei)) {
+		__u64 stats[2];
+
+		read_stats(skel, stats);
+		printf("0=%llu 1=%llu 2=%llu 3=%llu 4=%llu dsq_moves=%llu queue_moves=%llu\n", stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], stats[6]);
+		fflush(stdout);
+		sleep(1);
+	}
+
+	bpf_link__destroy(link);
+	ecode = UEI_REPORT(skel, uei);
+	scx_storm__destroy(skel);
+
+	if (UEI_ECODE_RESTART(ecode))
+		goto restart;
+	return 0;
+}
