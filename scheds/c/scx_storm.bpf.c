25ea91d8f7fea1f31cf426561b432180fb9cf76a
Christian Loehle <christian.loehle@arm.com>
2025-10-02 08:46:41 +0100

    scx_storm: Attempt observing ops.dequeue()
    In an attempt to prevent inserting a migration_disabled task into
    a local DSQ, observe the enqueue()/runnable() status of each task,
    if it has been dequeue()'d previously we might see it becoming
    migration_disabled asynchronously.
    
    Unfortunately this still doesn't solve the occasional
    migration_disabled LOCAL_ON DSQ insert.
    
    Signed-off-by: Christian Loehle <christian.loehle@arm.com>

--- scheds/c/scx_storm.bpf.c
+++ scheds/c/scx_storm.bpf.c
@@ -35,6 +35,18 @@ struct {
 	__type(value, s32);
 } storm_q SEC(".maps");
 
+struct task_ctx {
+	bool runnable;
+	bool in_qmap;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_TASK_STORAGE);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__type(key, int);
+	__type(value, struct task_ctx);
+} task_ctx_stor SEC(".maps");
+
 static void stat_inc(u32 idx)
 {
 	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
@@ -86,9 +98,20 @@ s32 BPF_STRUCT_OPS(storm_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wa
 void BPF_STRUCT_OPS(storm_enqueue, struct task_struct *p, u64 enq_flags)
 {
 	s32 pid = p->pid;
+	struct task_ctx *tctx = bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
 
-	if (coinflip(2) && !bpf_map_push_elem(&storm_q, &pid, 0))
+	if (!tctx) {
+		scx_bpf_error("Unable to find task ctx");
 		return;
+	}
+	/* tctx->runnable = 1; Not necessary? doesn't change behaviour anyway. */
+	if (tctx->in_qmap)
+		return;
+
+	if (coinflip(2) && !bpf_map_push_elem(&storm_q, &pid, 0)) {
+		tctx->in_qmap = 1;
+		return;
+	}
 	scx_bpf_dsq_insert(p, random_dsq(), SCX_SLICE_DFL, enq_flags);
 }
 
@@ -110,15 +133,21 @@ void BPF_STRUCT_OPS(storm_dispatch, s32 cpu, struct task_struct *prev)
 				scx_bpf_error("Lost task_struct with pid %d", pid);
 				return;
 			}
+			struct task_ctx *tctx = bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
+			if (!tctx) {
+				bpf_task_release(p);
+				scx_bpf_error("Unable to find task ctx");
+				return;
+			}
+			tctx->in_qmap = 0;
 			stat_inc(MAX_DSQS+1);
-			if (!bpf_cpumask_test_cpu(cpu, p->cpus_ptr) || is_migration_disabled(p))
+			if (!bpf_cpumask_test_cpu(cpu, p->cpus_ptr) || is_migration_disabled(p) || !tctx->runnable) {
 				scx_bpf_dsq_insert(p, random_dsq(), SCX_SLICE_DFL, 0);
-			else {
+			} else {
 				scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, SCX_SLICE_DFL, 0);
 				bpf_task_release(p);
 				break;
 			}
-
 			bpf_task_release(p);
 		}
 		if (scx_bpf_dsq_move_to_local(random)) {
@@ -131,6 +160,17 @@ void BPF_STRUCT_OPS(storm_dispatch, s32 cpu, struct task_struct *prev)
 
 void BPF_STRUCT_OPS(storm_running, struct task_struct *p)
 {
+	struct task_ctx *tctx = bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
+
+	if (!tctx) {
+		scx_bpf_error("Unable to find task ctx");
+		return;
+	}
+
+	if (!tctx->runnable)
+		scx_bpf_error("Running not runnable");
+	else if (tctx->in_qmap)
+		scx_bpf_error("Running qmap task?");
 }
 
 void BPF_STRUCT_OPS(storm_stopping, struct task_struct *p, bool runnable)
@@ -144,6 +184,48 @@ void BPF_STRUCT_OPS(storm_tick, struct task_struct *p)
 {
 	if (coinflip(3))
 		p->scx.slice = 0;
+}
+
+void BPF_STRUCT_OPS(storm_runnable, struct task_struct *p)
+{
+	struct task_ctx *tctx = bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
+
+	if (!tctx) {
+		scx_bpf_error("Unable to find task ctx");
+		return;
+	}
+	tctx->runnable = 1;
+}
+
+void BPF_STRUCT_OPS(storm_dequeue, struct task_struct *p)
+{
+	struct task_ctx *tctx = bpf_task_storage_get(&task_ctx_stor, p, 0, 0);
+
+	if (!tctx) {
+		scx_bpf_error("Unable to find task ctx");
+		return;
+	}
+
+	if (tctx->runnable == 0)
+		scx_bpf_error("Spurious dequeue for %d", p->pid);
+	tctx->runnable = 0;
+}
+
+
+s32 BPF_STRUCT_OPS(storm_init_task, struct task_struct *p, struct scx_init_task_args *args)
+{
+	struct task_ctx *tctx;
+
+	/*
+	 * @p is new. Let's ensure that its task_ctx is available. We can sleep
+	 * in this function and the following will automatically use GFP_KERNEL.
+	 */
+	tctx = bpf_task_storage_get(&task_ctx_stor, p, 0,
+				    BPF_LOCAL_STORAGE_GET_F_CREATE);
+	if (!tctx)
+		return -ENOMEM;
+
+	return 0;
 }
 
 void BPF_STRUCT_OPS(storm_enable, struct task_struct *p)
@@ -182,6 +264,9 @@ SCX_OPS_DEFINE(storm_ops,
 	       .running			= (void *)storm_running,
 	       .stopping		= (void *)storm_stopping,
 	       .tick			= (void *)storm_tick,
+	       .dequeue			= (void *)storm_dequeue,
+	       .runnable		= (void *)storm_runnable,
+	       .init_task		= (void *)storm_init_task,
 	       .enable			= (void *)storm_enable,
 	       .init			= (void *)storm_init,
 	       .exit			= (void *)storm_exit,
